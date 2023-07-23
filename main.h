6d1dce3a7cacea0fb0a2547ecece43a23f2cfcd2
Goodness <goodnessabisoye@gmail.com>
2023-07-23 06:06:45 -0700

    c

--- main.h
+++ main.h
@@ -6,4 +6,6 @@
 #include <string.h>
 #include <stdarg.h>
 int _printf(const char *format, ...);
+int print_string(va_list types, char buffer[],                     int flags, int width, int precision, int size)
+int print_binary(va_list types, char buffer[],                     int flags, int width, int precision, int size)
 #endif
